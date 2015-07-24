42d2c16b98103d7a0b80cdf17f00f1d751e2b297
Tomasz Szulc <mail@szulctomasz.com>
2015-07-24 23:41:58 +0200

    Create project

---
+++ Game-2048/Game-2048/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Game-2048
+//
+//  Created by Tomasz Szulc on 25/07/15.
+//  Copyright (c) 2015 Tomasz Szulc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
